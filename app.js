24e947f595b2b50e6045a8bcacdba289b9f57aab
Lindsay Esterman <lindsayesterman@gmail.com>
2022-01-01 17:40:56 -0500

    remove counseling tip, add teamn'up

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 40,
+        "value": 32,
         "density": {
           "enable": true,
           "value_area": 800
